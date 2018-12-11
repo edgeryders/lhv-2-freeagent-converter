93cd9fe30b95dc9b58975d14ac7c900e407882fa
Alberto Cottica <alberto@cottica.net>
2018-12-11 23:08:17 +0100

    refactoring

--- convert_LHV_statement_python_2x.py
+++ convert_LHV_statement_python_2x.py
@@ -20,72 +20,47 @@ def generate_report(currency, transactions):
     '''
     ## iterate on nonEURrows to find the transaction in that currency
     outListCurrency = []
-    for transaction in transactions:
-        if transaction['Currency'] == currency:
-            outListItem = {} # each item in the list is a dict
-            formattedDate = row['Date'][8:] + row['Date'][4:8] + row['Date'][:4]
-            outListItem['Date'] = formattedDate
-            amount = str(row['Amount'])
-            outListItem['Amount'] = amount
-            formattedDescription = row['Description'] + ' Sender/receiver: ' + row['Sender/receiver name'] + ' ' + row['Sender/receiver account']
-            outListItem['Description'] = formattedDescription
+    for row in transactions:
+        outListItem = {} # each item in the list is a dict
+        if row['Currency'] == currency:
+            outListItem['Date'] = row['Date']
+            outListItem['Amount'] = row['Amount']
+            outListItem['Description'] = row['Description']
             outListCurrency.append(outListItem)
     return outListCurrency
-            
-    
-    
-  
+ 
 
 tk.Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
 filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
 directory = askdirectory(title = "Choose a directory") # shows a "choose directory" dialog box
-outFilename = directory + '/Transactions_from_LHV.csv'
 
 outList = [] # initialize the list where to store the output
-nonEURrows = [] # a list of raw non-EUR payments
-nonEURcurrencies = [] # a list of non-EUR currencies encountered
+currencies = [] # a list of non-EUR currencies encountered
 
 with open (filename) as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
-        if row['Currency'] == 'EUR':
-            outListItem = {} # each item in the list is a dict
-            formattedDate = row['Date'][8:] + row['Date'][4:8] + row['Date'][:4]
-            outListItem['Date'] = formattedDate
-            amount = str(row['Amount'])
-            outListItem['Amount'] = amount
-##        if row['Debit/Credit (D/C)'] == "C":
-##            formattedAmount = round(float(row['Amount']), 2)
-##        else:
-##            formattedAmount = -1 * (round(float(row['Amount']), 2))
-##        outListItem['Amount'] = formattedAmount
-            formattedDescription = row['Description'] + ' Sender/receiver: ' + row['Sender/receiver name'] + ' ' + row['Sender/receiver account']
-            outListItem['Description'] = formattedDescription
-            outList.append(outListItem)
-        else:
-            nonEURrows.append(row)
-            nonEURcurrencies.append(row['Currency'])
-
-    if len(nonEURcurrencies) > 0: # we have encountered at least one non-EUR currency
-        for currency in nonEURcurrencies:
-            outFilenameCurrency = directory + '/transactions_in_' + currency + '.csv'
-            rows_to_file = generate_report(currency, nonEURrows)
-            with open (outFilenameCurrency, 'w') as outFile:
-                for item in rows_to_file:
-            ##        oneRow = str(item ['Date']) + ', ' + str(Decimal(item ['Amount']).quantize(Decimal('.01'))) + ', ' + item ['Description'] + '\n'
-                    oneRow = str(item ['Date']) + ', ' + item['Amount'] + ', ' + item ['Description'].replace(',', '') + '\n'
-                    outFile.write(oneRow)
+        outListItem = {} # each item in the list is a dict
+        formattedDate = row['Date'][8:] + row['Date'][4:8] + row['Date'][:4]
+        outListItem['Date'] = formattedDate
+        amount = str(row['Amount'])
+        outListItem['Amount'] = amount
+        formattedDescription = row['Description'] + ' Sender/receiver: ' + row['Sender/receiver name'] + ' ' + row['Sender/receiver account']
+        outListItem['Description'] = formattedDescription
+        outListItem['Currency'] = row['Currency']
+        outList.append(outListItem)
+        curr = row['Currency']
+        if curr not in currencies:
+            currencies.append(curr)
+        
+for currency in currencies:
+    outFilename = directory + '/transactions_in_' + currency + '.csv'
+    rows_to_file = generate_report(currency, outList)
+    with open (outFilename, 'w') as outFile:
+        for item in rows_to_file:
+            oneRow = str(item ['Date']) + ', ' + item['Amount'] + ', ' + item ['Description'].replace(',', '') + '\n'
+            outFile.write(oneRow)
             
-
-
-# save outList (the main list with only EUR transactions) as a CSV file. I cannot use dictWriter because FreeAgent wants a specific order for columns.
-
-with open (outFilename, 'w') as outFile:
-    for item in outList:
-##        oneRow = str(item ['Date']) + ', ' + str(Decimal(item ['Amount']).quantize(Decimal('.01'))) + ', ' + item ['Description'] + '\n'
-        oneRow = str(item ['Date']) + ', ' + item['Amount'] + ', ' + item ['Description'].replace(',', '') + '\n'
-        outFile.write(oneRow)
-    
     
 
         
--- convert_LHV_statement_python_3x.py
+++ convert_LHV_statement_python_3x.py
@@ -5,7 +5,7 @@ from sys import version_info
 from sys import exit
 if version_info.major == 2:
     # We are using Python 2.x
-    sys.exit ("Please refer to the cersion of the script for Python 2: https://github.com/edgeryders/lhv-2-freeagent-converter")
+    sys.exit ("Please refer to the version of the script for Python 2: https://github.com/edgeryders/lhv-2-freeagent-converter")
 # We are using Python 3.
 import tkinter
 import csv
@@ -21,15 +21,12 @@ def generate_report(currency, transactions):
     '''
     ## iterate on nonEURrows to find the transaction in that currency
     outListCurrency = []
-    for transaction in transactions:
-        if transaction['Currency'] == currency:
-            outListItem = {} # each item in the list is a dict
-            formattedDate = row['Date'][8:] + row['Date'][4:8] + row['Date'][:4]
-            outListItem['Date'] = formattedDate
-            amount = str(row['Amount'])
-            outListItem['Amount'] = amount
-            formattedDescription = row['Description'] + ' Sender/receiver: ' + row['Sender/receiver name'] + ' ' + row['Sender/receiver account']
-            outListItem['Description'] = formattedDescription
+    for row in transactions:
+        outListItem = {} # each item in the list is a dict
+        if row['Currency'] == currency:
+            outListItem['Date'] = row['Date']
+            outListItem['Amount'] = row['Amount']
+            outListItem['Description'] = row['Description']
             outListCurrency.append(outListItem)
     return outListCurrency
 
@@ -37,47 +34,30 @@ def generate_report(currency, transactions):
 tkinter.Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
 filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
 directory = askdirectory(title = "Choose a directory") # shows a "choose directory" dialog box
-outFilename = directory + '/Transactions_from_LHV.csv'
 
-outList = [] # initialize the dictionary where to store the output
-nonEURrows = [] # a list of raw non-EUR payments
-nonEURcurrencies = [] # a list of non-EUR currencies encountered
+outList = [] # initialize the list where to store the output
+currencies = [] # a list of non-EUR currencies encountered
 
-with open (filename) as csvfile: # this part generates the list for the main EUR CSV file
+with open (filename, 'r', encoding='utf-8') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
-        if row['Currency'] == 'EUR':
-            outListItem = {} # each item in the list is a dict
-            formattedDate = row['Date'][8:] + row['Date'][4:8] + row['Date'][:4]
-            outListItem['Date'] = formattedDate
-            amount = str(row['Amount'])
-            outListItem['Amount'] = amount
-            formattedDescription = row['Description'] + ' Sender/receiver: ' + row['Sender/receiver name'] + ' ' + row['Sender/receiver account']
-            outListItem['Description'] = formattedDescription
-            outList.append(outListItem)
-        else: # this part provides for the non-EUR transaction files
-            nonEURrows.append(row)
-            nonEURcurrencies.append(row['Currency'])
-            
-    if len(nonEURcurrencies) > 0: # we have encountered at least one non-EUR currency
-        for currency in nonEURcurrencies:
-            outFilenameCurrency = directory + '/transactions_in_' + currency + '.csv'
-            rows_to_file = generate_report(currency, nonEURrows)
-            with open (outFilenameCurrency, 'w') as outFile:
-                for item in rows_to_file:
-            ##        oneRow = str(item ['Date']) + ', ' + str(Decimal(item ['Amount']).quantize(Decimal('.01'))) + ', ' + item ['Description'] + '\n'
-                    oneRow = str(item ['Date']) + ', ' + item['Amount'] + ', ' + item ['Description'].replace(',', '') + '\n'
-                    outFile.write(oneRow)
-
-
-# save outList (the main list with only EUR transactions) as a CSV file. I cannot use dictWriter because FreeAgent wants a specific order for columns.with open (outFilename, 'w') as outFile:
-    for item in outList:
-##        oneRow = str(item ['Date']) + ', ' + str(Decimal(item ['Amount']).quantize(Decimal('.01'))) + ', ' + item ['Description'] + '\n'
-        oneRow = str(item ['Date']) + ', ' + item['Amount'] + ', ' + item ['Description'].replace(',', '') + '\n'
-        outFile.write(oneRow)
-    
-    
-
+        outListItem = {} # each item in the list is a dict
+        formattedDate = row['Date'][8:] + row['Date'][4:8] + row['Date'][:4]
+        outListItem['Date'] = formattedDate
+        amount = str(row['Amount'])
+        outListItem['Amount'] = amount
+        formattedDescription = row['Description'] + ' Sender/receiver: ' + row['Sender/receiver name'] + ' ' + row['Sender/receiver account']
+        outListItem['Description'] = formattedDescription
+        outListItem['Currency'] = row['Currency']
+        outList.append(outListItem)
+        curr = row['Currency']
+        if curr not in currencies:
+            currencies.append(curr)
         
-
-
+for currency in currencies:
+    outFilename = directory + '/transactions_in_' + currency + '.csv'
+    rows_to_file = generate_report(currency, outList)
+    with open (outFilename, 'w', encoding='utf-8') as outFile:
+        for item in rows_to_file:
+            oneRow = str(item ['Date']) + ', ' + item['Amount'] + ', ' + item ['Description'].replace(',', '') + '\n'
+            outFile.write(oneRow)
